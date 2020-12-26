3e198640d9b6c8b75663f9486c909f96e1d62198
Seyhan Tüzün <65950638+seyhantzn@users.noreply.github.com>
2020-12-26 14:44:51 +0300

    Add files via upload

---
+++ homepage1.py
@@ -0,0 +1,8 @@
+a = input("Please enter your name:")
+b = int(input("Please enter your age:"))
+c = float(input("Please enter your height:"))
+d = int(input("Please enter your weight :"))
+e = list(input("Please enter your surname, we will split the word into letters :"))
+
+print(f" Your is Name : {a}\n Your Age is : {b}\n Your Height is : {c}\n Your Weight is : {d}\n Your Surname is : {e}\n")
+print(f" First Type is : {type(a)}\n Second Type is {type(b)}\n Third Type is : {type(c)}\n  Fourth Type is : {type(d)}\n Fifth Type is :{type(e)}\n")
